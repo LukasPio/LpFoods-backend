2989a43241285fb9903e3d6dd8c44e0779d479b3
LukasPio <lucaspio.galvao@gmail.com>
2024-02-20 01:28:58 -0300

    Add many new requisitions and notifications after retrieve any request

--- src/main/java/com/lucas/fastFood/controller/FoodController.java
+++ src/main/java/com/lucas/fastFood/controller/FoodController.java
@@ -11,20 +11,43 @@ import java.util.List;
 
 @RestController
 @RequestMapping("foods")
+@CrossOrigin(origins = "*", allowedHeaders = "*")
 public class FoodController {
     @Autowired
     private FoodRepository foodRepository;
 
-    @CrossOrigin(origins = "*", allowedHeaders = "*")
     @GetMapping
     public List<FoodResponseDTO> getAll() {
+        System.out.println("Get requisition received");
         return foodRepository.findAll().stream().map(FoodResponseDTO::new).toList();
     }
 
-    @CrossOrigin(origins = "*", allowedHeaders = "*")
+    @GetMapping("/{id}")
+    public List<FoodResponseDTO> getById(@PathVariable Long id) {
+        System.out.println("Get requisition received");
+        return foodRepository.findById(id).stream().map(FoodResponseDTO::new).toList();
+    }
+
     @PostMapping
-    public void saveFood(@RequestBody FoodRequestDTO foodData) {
-        foodRepository.save(new Food(foodData));
+    public void saveFoods(@RequestBody List<FoodRequestDTO> foodDataList) {
+        System.out.println("Post requisition received");
+        foodDataList.forEach((foodData) -> foodRepository.save(new Food(foodData)));
+    }
+
+    @DeleteMapping("/{id}")
+    public void removeFoodById(@PathVariable Long id) {
+        System.out.println("Delete requisition received");
+        foodRepository.deleteById(id);
+    }
+
+    @PutMapping("/{id}")
+    public void updateFoodById(@PathVariable Long id, @RequestBody FoodRequestDTO foodData) {
+        System.out.println("Put requisition received");
+        Food oldFood = foodRepository.findById(id).orElseThrow(() -> new IllegalArgumentException("Food with id: "+id+" was not found."));
+        oldFood.setName(foodData.name());
+        oldFood.setImage(foodData.image());
+        oldFood.setPrice(foodData.price());
+        foodRepository.save(oldFood);
     }
 
 }
